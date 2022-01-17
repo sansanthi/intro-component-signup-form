f6b398c07f2bc34b341675b35cd1a8dc7e64d310
San San Thi <sansanthi1996@gmail.com>
2022-01-18 01:06:46 +0630

    responsive and input error message

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const submitBtn = document.querySelector('.btn');
+
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
 const email = document.getElementById('email');
+const submitBtn = document.querySelector('.btn');
 let emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const form = document.querySelector('.signup');
 
 
 submitBtn.addEventListener('click', function(e){
     e.preventDefault();
-    
+    //check input fields
     if(firstName.value === '') {
+        firstName.value = '';
         showErrorMessage(firstName, "First Name cannot be empty!");
     }
     if(lastName.value === '') {
@@ -19,14 +20,15 @@ submitBtn.addEventListener('click', function(e){
     if(email.value === ''){
         showErrorMessage(email, "Email cannot be empty!");
     } else if (!emailRegExp.test(email.value)){
+        email.style.color = 'hsl(0, 100%, 74%) ';
         showErrorMessage(email, "Look like this is not an email!");
     }
     if(password.value === '') {
         showErrorMessage(password, "Password cannot be empty");
     }
-    form.reset();
 });
 
+//error message
 function showErrorMessage(inputValue, message) {
     const inputControl = inputValue.parentElement;
     inputControl.classList.add('error');
